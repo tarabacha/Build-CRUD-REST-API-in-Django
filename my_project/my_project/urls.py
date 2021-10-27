1aac38c2a4e930244001b85b85b5fad6d596e09f
starbacha <73193663+starbacha@users.noreply.github.com>
2021-10-27 19:48:11 +0530

    Update urls.py
    Remove the admin functionality to make sure admin structure isn't accessible.

--- my_project/my_project/urls.py
+++ my_project/my_project/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 URL Updated
 """
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path
 from my_app.views import EmployeeDetails, ListEmployee
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+#     path('admin/', admin.site.urls),
+    ## Removed the admin site access in order stio data leaks
     path('employeedetails/', EmployeeDetails),
     path('listemployee/', ListEmployee.as_view()),
     path('updateemployee/<id>', UpdateEmployee.as_view())
